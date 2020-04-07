ee724aa78ce1831217de7c58cdba66ed91c0e92a
Nguyen Vu Linh <nguyenvulinh99@gmail.com>
2020-04-07 16:40:25 +0700

    Add middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,22 +34,6 @@ module.exports.get = function(req, res) {
 };
 
 module.exports.postCreate = function(req, res) {
-  var errors = [];
-  if(!req.body.name){
-    errors.push('Name is required.')
-  }
-  
-  if(!req.body.phone){
-    errors.push('Phone is required.')
-  }
-  if(errors.length){
-    res.render('users/create',{
-      errors: errors,
-      values: req.body
-    });
-    return;
-  }
-
   db.get('users').push(req.body).write();
   res.redirect('/users');
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+
 var router = express.Router();
 
 router.get('/', controller.index);
@@ -11,6 +13,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;

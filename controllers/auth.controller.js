71c5759027ae1d398efcf3beb845c6e49e141f62
Nguyen Vu Linh <nguyenvulinh99@gmail.com>
2020-04-08 20:51:41 +0700

    Test hash password with md5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
+var md5 = require('md5');
 var db = require('../db');
-
 
 module.exports.login = function(req, res) {
   res.render('auth/login');
@@ -21,7 +21,9 @@ module.exports.postLogin = function(req, res) {
     return;
   }
 
-  if (user.password !== password) {
+  var hashedPassword = md5(password);
+
+  if (user.password !== hashedPassword) {
     res.render('auth/login', {
       errors: [
         'Wrong password.'

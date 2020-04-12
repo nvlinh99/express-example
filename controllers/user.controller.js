d5c2069a9f9304b4c42b40610af92c7c187bcb8b
Nguyen Vu Linh <nguyenvulinh99@gmail.com>
2020-04-12 21:37:28 +0700

    Add upload file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -37,6 +37,9 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
   req.body.id = shortid.generate();
+
+  req.body.avatar = req.file.path.split('\\').slice(1).join('/');
+
   db.get('users').push(req.body).write();
   res.redirect('/users');
 };

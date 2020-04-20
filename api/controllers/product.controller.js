f894672a99938b5d029b8a70ee817151a8fc15ca
Linh Nguyen <nguyenvulinh99@gmail.com>
2020-04-20 15:19:28 +0700

    add simple REST APT

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -16,3 +16,8 @@ module.exports.index = async function(req, res) {
     var products = await Product.find();
 	res.json(products);
 };
+
+module.exports.create = async function(req, res) {
+    var products = await Product.create(req.body);
+    res.json(products);
+};
